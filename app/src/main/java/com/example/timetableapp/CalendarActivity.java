a4f6f5b62379bfcb6e93472dbc3a3559441e76e7
29574196 <29574196@student.g.nwu.ac.za>
2019-08-26 21:13:57 +0200

    Calendar view added

--- app/src/main/java/com/example/timetableapp/CalendarActivity.java
+++ app/src/main/java/com/example/timetableapp/CalendarActivity.java
@@ -1,14 +1,31 @@
 package com.example.timetableapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
 
 public class CalendarActivity extends AppCompatActivity {
+    private static final String TAG = "CalendarActivity";
+
+    private CalendarView calView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        calView = (CalendarView) findViewById(R.id.calendarView);
+
+       calView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+           @Override
+           public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+               String date = i + "/" +i1 + "/" + i2;
+               Log.d(TAG, "onSelectedDayChange: date:"+ date );
+           }
+       });
+
     }
 }
