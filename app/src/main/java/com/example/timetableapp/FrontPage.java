f2728dff6b3c76053c947df824d913d26c3fce31
29574196 <29574196@student.g.nwu.ac.za>
2019-08-26 21:02:33 +0200

    Calendar PAge is made

--- app/src/main/java/com/example/timetableapp/FrontPage.java
+++ app/src/main/java/com/example/timetableapp/FrontPage.java
@@ -2,13 +2,32 @@ package com.example.timetableapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FrontPage extends AppCompatActivity {
+    private TextView date;
+    private Button btnGoCalendar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page);
+
+        date = (TextView) findViewById(R.id.txtDate);
+        btnGoCalendar = (Button) findViewById(R.id.btnCal);
+
+        btnGoCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FrontPage.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
