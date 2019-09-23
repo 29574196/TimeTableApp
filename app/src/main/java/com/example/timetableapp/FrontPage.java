5bcf83e7c585d01c63bd41bb0f271f7812336683
29574196 <29574196@student.g.nwu.ac.za>
2019-09-23 21:48:34 +0200

    Added Navigation bar details

--- app/src/main/java/com/example/timetableapp/FrontPage.java
+++ app/src/main/java/com/example/timetableapp/FrontPage.java
@@ -1,16 +1,22 @@
 package com.example.timetableapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class FrontPage extends AppCompatActivity {
-    private TextView txtDate;
-    private Button btnGoCalendar;
+
+
+    private DrawerLayout mDrawerlayout;
+    private ActionBarDrawerToggle mToggle;
 
 
     @Override
@@ -18,13 +24,30 @@ public class FrontPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page);
 
-        Intent incoming_Date_intent = getIntent();
-        String date = incoming_Date_intent.getStringExtra("date");
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.navDrawerLayout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerlayout,R.string.open,R.string.close);
+        mDrawerlayout.addDrawerListener(mToggle);
+
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
 
 
 
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item))
+        {
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
 
 
     }
