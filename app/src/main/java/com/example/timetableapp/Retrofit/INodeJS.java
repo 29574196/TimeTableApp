1284fad8984cd602dc6bc8fea9d7139a3dbe8a1c
TSephiri <31255086@student.g.nwu.ac.za>
2019-09-24 16:19:04 +0200

    Method created to pass login info to backend

--- app/src/main/java/com/example/timetableapp/Retrofit/INodeJS.java
+++ app/src/main/java/com/example/timetableapp/Retrofit/INodeJS.java
@@ -1,4 +1,16 @@
 package com.example.timetableapp.Retrofit;
 
+import io.reactivex.Observable;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface INodeJS {
+        @POST("login")
+        @FormUrlEncoded
+        Observable <String> userLogin(
+                @Field("add_student") String student,
+                @Field("add_password") String password
+        );
+
 }
