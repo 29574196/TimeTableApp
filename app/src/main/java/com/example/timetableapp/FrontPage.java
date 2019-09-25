ed2d16e02d5438e3dfb97e7ba6d77ae202d9701b
29574196 <29574196@student.g.nwu.ac.za>
2019-09-25 12:32:29 +0200

    Added navigation click on listener but not responding

--- app/src/main/java/com/example/timetableapp/FrontPage.java
+++ app/src/main/java/com/example/timetableapp/FrontPage.java
@@ -6,17 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.media.MediaExtractor;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class FrontPage extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class FrontPage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     private DrawerLayout mDrawerlayout;
     private ActionBarDrawerToggle mToggle;
+    private NavigationView navView;
 
 
     @Override
@@ -25,25 +29,45 @@ public class FrontPage extends AppCompatActivity {
         setContentView(R.layout.activity_front_page);
 
         mDrawerlayout = (DrawerLayout) findViewById(R.id.navDrawerLayout);
-        mToggle = new ActionBarDrawerToggle(this,mDrawerlayout,R.string.open,R.string.close);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
         mDrawerlayout.addDrawerListener(mToggle);
+        navView = (NavigationView) findViewById(R.id.nav_view);
+
+        navView.setNavigationItemSelectedListener(this);
+
 
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.navDaily:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_container,
+                        new DailyviewFragment()).commit();
+                break;
+            case R.id.navAppoint:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_container,
+                        new AppointmentFragment()).commit();
+                break;
+            case R.id.navModule:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frag_container,
+                        new ModuleFragment()).commit();
+                break;
 
 
+        }
 
-
-
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        if(mToggle.onOptionsItemSelected(item))
-        {
+        if (mToggle.onOptionsItemSelected(item)) {
             return true;
 
         }
