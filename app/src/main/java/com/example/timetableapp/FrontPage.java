026d69cdda0e690d40c722bdd75fe5949db3a0d5
TSephiri <31255086@student.g.nwu.ac.za>
2019-10-11 16:51:27 +0200

    added functionality to get class method and displays toast with info

--- app/src/main/java/com/example/timetableapp/FrontPage.java
+++ app/src/main/java/com/example/timetableapp/FrontPage.java
@@ -10,13 +10,28 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.media.MediaExtractor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.timetableapp.Retrofit.ClassModel;
+import com.example.timetableapp.Retrofit.INodeJS;
+import com.example.timetableapp.Retrofit.RetrofitClient;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.List;
+
+import io.reactivex.disposables.CompositeDisposable;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 import retrofit2.http.HEAD;
 
 
@@ -28,8 +43,25 @@ public class FrontPage extends AppCompatActivity implements NavigationView.OnNav
     private DrawerLayout mDrawerlayout;
     private ActionBarDrawerToggle mToggle;
     private NavigationView navView;
+    private List<ClassModel> classList;
+
+    INodeJS myAPI;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    @Override
+    protected void onStop() {
+        compositeDisposable.clear();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        compositeDisposable.clear();
+        super.onDestroy();
+    }
 
     private TextView headerStudentNo,headerEmail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,21 +77,56 @@ public class FrontPage extends AppCompatActivity implements NavigationView.OnNav
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        //headerEmail.setText("");
-        //header = mDrawerlayout;
-        //headerStudentNo = (TextView) findViewById(R.id.studentNumber);
-        //headerStudentNo.setText("heyyyy");
-        //headerEmail = (TextView) findViewById(R.id.emailAddress);
-
-
-
         navView.setNavigationItemSelectedListener(this);
-
 
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+        //initialize api
+        //Retrofit retrofit = RetrofitClient.getInstance();
+
+
+        //********************************************************
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("http://192.168.43.224:3000/")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+        myAPI = retrofit.create(INodeJS.class);
+        //********************************************************
+        getClasses();
+
+    }
+
+    public void getClasses()
+    {
+        Call<List<ClassModel>> call = myAPI.getClassInfo("12121212");
+
+        Toast.makeText(FrontPage.this, "Uhm : hey" , Toast.LENGTH_SHORT).show();
+
+        call.enqueue(new Callback<List<ClassModel>>() {
+            @Override
+            public void onResponse(Call<List<ClassModel>> call, Response<List<ClassModel>> response) {
+
+               classList = response.body();
+
+               String s="";
+               for(ClassModel c : classList)
+                {
+                    s += "  " +  c.getModule();
+
+                    //creating new activity
+                }
+               //Test to see if data was retrieved successfully
+                Toast.makeText(FrontPage.this, "Uhm : " + s, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<List<ClassModel>> call, Throwable t) {
+                Log.i("log ",t.getMessage());
+            }
+        });
     }
 
     @Override
