d5513ea164f3ab3ce9fb6a7d36387398ab13e408
Andries Ngeke <28265548@student.g.nwu.ac.za>
2019-10-23 21:49:45 +0200

    Added Thursday and get module-CODE

--- app/src/main/java/com/example/timetableapp/Retrofit/RetrofitClient.java
+++ app/src/main/java/com/example/timetableapp/Retrofit/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit instance;
-
+/////////
     public static Retrofit getInstance(){
         if(instance == null)
             instance = new Retrofit.Builder()
